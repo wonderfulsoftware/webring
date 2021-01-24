5624d2965649bb0a177bd0ee4f19b676c604c0df
dtinth on Surface Pro 7 <dtinth@spacet.me>
2021-01-24 20:08:27 +0700

    Aggregate summary

--- automation/usage-publisher/index.js
+++ automation/usage-publisher/index.js
@@ -1,18 +1,25 @@
 const stats = require("../../tmp/amplitude.json").data
-const targetDate = "2021-01-18"
-const targetIndex = stats.xValues.indexOf(targetDate)
-if (targetIndex === -1) {
-  throw new Error(`Data for ${targetDate} not found.`)
-}
-const stat = {}
-for (const [i, meta] of stats.seriesMeta.entries()) {
-  const [to, from] = meta.eventGroupBys
-  const count = stats.series[i][targetIndex].value
-  if (count > 0 && from !== "(none)") {
-    if (!stat[from]) {
-      stat[from] = {}
+// const targetDate = "2021-01-18"
+// const targetIndex = stats.xValues.indexOf(targetDate)
+// if (targetIndex === -1) {
+//   throw new Error(`Data for ${targetDate} not found.`)
+// }
+
+for (const [targetIndex, targetDate] of stats.xValues.entries()) {
+  if (targetDate < "2021-01-24") continue
+  const stat = {}
+  const summary = { total: 0, network: 0, external: 0 }
+  for (const [i, meta] of stats.seriesMeta.entries()) {
+    const [to, from] = meta.eventGroupBys
+    const count = stats.series[i][targetIndex].value
+    if (count > 0 && from !== "(none)") {
+      if (!stat[from]) {
+        stat[from] = {}
+      }
+      stat[from][to] = count
     }
-    stat[from][to] = count
+    summary.total += count
+    summary[from === "(none)" ? "external" : "network"] += count
   }
+  console.log(targetDate, summary)
 }
-console.log(stat)
