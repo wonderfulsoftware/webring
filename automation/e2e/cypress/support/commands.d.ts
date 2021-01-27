e60091001bb1b8d8136fd4b81854ffa0e619a2b5
Thai Pangsakulyanont <dtinth@spacet.me>
2021-01-27 16:55:56 +0000

    Add comments

--- automation/e2e/cypress/support/commands.d.ts
+++ automation/e2e/cypress/support/commands.d.ts
@@ -1,3 +1,4 @@
+// See: https://docs.cypress.io/guides/tooling/typescript-support.html#Types-for-custom-commands
 declare namespace Cypress {
   interface Chainable {
     visitRoute(value: string): Chainable
--- automation/e2e/cypress/support/commands.js
+++ automation/e2e/cypress/support/commands.js
@@ -1,15 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
+// See: https://on.cypress.io/custom-commands
 Cypress.Commands.add("visitRoute", (hash) => {
   cy.visit("/?test=" + Date.now() + hash)
 })
@@ -17,16 +6,3 @@ Cypress.Commands.add("visitRoute", (hash) => {
 Cypress.Commands.add("shouldAutomaticallySelectPage", (siteName) => {
   cy.get("h2").should("contain.text", siteName)
 })
-
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
