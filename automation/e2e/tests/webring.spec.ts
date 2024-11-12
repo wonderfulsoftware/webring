2ececcadeffe9d7a845ff42a453cc17b3a18ea5c
Thai Pangsakulyanont <dtinth@spacet.me>
2024-11-13 02:27:06 +0700

    hopefully fix the specs

--- automation/e2e/tests/webring.spec.ts
+++ automation/e2e/tests/webring.spec.ts
@@ -1,90 +1,90 @@
-import { test, expect, Page } from "@playwright/test"
+import { test, expect, Page } from "@playwright/test";
 
 test.describe("Website information", () => {
   test.beforeEach(async ({ page }) => {
-    await visit(page, "#/dt.in.th")
-  })
+    await visit(page, "#/dt.in.th");
+  });
 
   test("Website title is shown", async ({ page }) => {
-    await expect(page.locator("h2")).toHaveText("dt.in.th")
-  })
+    await expect(page.locator("h2")).toHaveText("dt.in.th");
+  });
 
   test("Website description is shown", async ({ page }) => {
     await expect(page.locator(".site-description")).toHaveText(
       "Thai Pangsakulyanont’s web site"
-    )
-  })
+    );
+  });
 
   test("Next button", async ({ page }) => {
-    await page.click("#next-button")
-    await assertHash(page, "#/notaboutcode.com")
-  })
+    await page.click("#next-button");
+    await assertHash(page, "#/notaboutcode.com");
+  });
 
   test("Previous button", async ({ page }) => {
-    await page.click("#previous-button")
-    await assertHash(page, "#/wonderful.software")
-  })
-})
+    await page.click("#previous-button");
+    await assertHash(page, "#/wonderful.software");
+  });
+});
 
 test.describe("Visiting directly", () => {
   test.beforeEach(async ({ page }) => {
-    await visit(page, "#")
-  })
+    await visit(page, "#");
+  });
 
   test("selects a random website", async ({ page }) => {
-    await assertHash(page, "#/monosor.dev")
-  })
+    await assertHash(page, "#/garden.narze.live");
+  });
 
   test("should send beacon when visiting that site", async ({ page }) => {
-    await page.click('[data-cy="go:monosor.dev"]')
+    await page.click('[data-cy="go:garden.narze.live"]');
     await assertBeacon(page, {
       action: "outbound",
-      site: "monosor.dev",
+      site: "garden.narze.live",
       referrer: "",
-    })
-  })
-})
+    });
+  });
+});
 
 test.describe("inbound links", () => {
   test.beforeEach(async ({ page }) => {
-    await visit(page, "#monosor.dev")
-  })
+    await visit(page, "#dt.in.th");
+  });
 
   test("should automatically advance to next page", async ({ page }) => {
-    await page.click('[data-cy="go:garden.narze.live"]')
-    await assertHash(page, "#/garden.narze.live")
-  })
+    await page.click('[data-cy="go:garden.narze.live"]');
+    await assertHash(page, "#/garden.narze.live");
+  });
 
   test("should send beacon upon entering", async ({ page }) => {
     await assertBeacon(page, {
       action: "inbound",
-      site: "monosor.dev",
-    })
-  })
+      site: "dt.in.th",
+    });
+  });
 
   test("should send beacon when visiting next site", async ({ page }) => {
-    await page.click('[data-cy="go:garden.narze.live"]')
+    await page.click('[data-cy="go:garden.narze.live"]');
     await assertBeacon(page, {
       action: "outbound",
       site: "garden.narze.live",
-      referrer: "monosor.dev",
-    })
-  })
-})
+      referrer: "dt.in.th",
+    });
+  });
+});
 
 const visit = (page: Page, route: string) =>
-  page.goto(`/?test=1&test_onboarded=1${route}`)
+  page.goto(`/?test=1&test_onboarded=1${route}`);
 
 const assertBeacon = async (page: Page, target: any) => {
   await expect
     .poll(async () => {
       return page.evaluate(() => {
-        return (window as any).WEBRING_TEST_MODE.sentBeacons
-      })
+        return (window as any).WEBRING_TEST_MODE.sentBeacons;
+      });
     })
-    .toContainEqual(expect.objectContaining(target))
-}
+    .toContainEqual(expect.objectContaining(target));
+};
 
 const assertHash = async (page: Page, hash: string) => {
-  await expect.poll(() => page.evaluate(() => window.location.hash)).toBe(hash)
-}
+  await expect.poll(() => page.evaluate(() => window.location.hash)).toBe(hash);
+};
